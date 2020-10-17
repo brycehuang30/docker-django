d7e679f37dd18015619d540a85184ee961194ff1
Hsin-Yu Huang <brayce1996@gmail.com>
2020-10-17 22:18:07 +0800

    Update production docker-compose with always restart config

--- backup_db.sh
+++ backup_db.sh
@@ -4,7 +4,7 @@ sudo docker run --rm -v docker-django_postgres_data:/dbdata -v $(pwd)/backup:/ba
 # Use following command to restore/apply backup.
 # I recommand not to make it a script, because it is a dangerous operation (overwrite the whole database)
 # Template:
-# sudo docker run --rm -v <volume_name>:/dbdata $(pwd):/backup ubuntu:latest bash -c "cd /dbdata && tar xvf /backup/<backup_filename> --strip 1"
+# sudo docker run --rm -v <volume_name>:/dbdata -v $(pwd):/backup ubuntu:latest bash -c "cd /dbdata && tar xvf /backup/<backup_filename> --strip 1"
 #
 # Example:
-# sudo docker run --rm -v docker-django_postgres_data:/dbdata $(pwd):/backup postgres:12.0-alpine bash -c "cd /dbdata && tar xvf /backup/backup.2020-08-20_03\:08\:06.tar --strip 1"
+# sudo docker run --rm -v docker-django_postgres_data:/dbdata -v $(pwd):/backup postgres:12.0-alpine bash -c "cd /dbdata && tar xvf /backup/backup.2020-08-20_03\:08\:06.tar --strip 1"
